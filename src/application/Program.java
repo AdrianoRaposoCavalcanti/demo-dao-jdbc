ac5c56fa79e7f3a3ba43c2cde8e89dc9121cc988
Adriano <sis.raposo@gmail.comm>
2025-04-30 10:16:24 -0300

    findByAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,26 +11,32 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		 
-	//	Department obj = new Department(1, "Books");
-		
-	//	Seller seller = new Seller(21,"Adriano","adr@gmail.com", new Date(), 3000.0, obj);
-		
+
+		// Department obj = new Department(1, "Books");
+
+		// Seller seller = new Seller(21,"Adriano","adr@gmail.com", new Date(), 3000.0,
+		// obj);
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-	
-		
+
 		System.out.println("=== Test 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		
+
 		System.out.println("\n=== Test 2: seller findByDepartmentId ===");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
-		
+
+		System.out.println("\n=== Test 3: seller findByAll ===");
+
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+
 	}
 
 }
